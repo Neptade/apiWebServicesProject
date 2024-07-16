8da5d6ac81e0fda0c9187c3573dd2c7c7c7fc6c4
Neptade <loic@lozolozo.com>
2024-07-16 19:40:01 +0200

    removed obselete google auth code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Room from "./pages/Room";
 import Profile from "./pages/Profile";
-import { GoogleLogin } from '@react-oauth/google';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
 	const makePage = (page) => (<>{page}</>);
 
-	const responseMessage = (response) => {
-        console.log(response);
-    };
-    const errorMessage = (error) => {
-        console.log(error);
-    };
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h2>React Google Login</h2>
-				<GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+			<a href="http://localhost:3001/auth/google">Sign in with Google</a>
 			</header>
 			<BrowserRouter>
 				<Routes>
